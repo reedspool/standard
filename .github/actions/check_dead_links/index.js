bd7b13d833abf3aaf22533dba99587c7051ec0e2
Reed Spool <reed@heysparkbox.com>
2021-05-31 00:28:15 -0600

    feat: add cluster to check via puppeteer

--- .github/actions/check_dead_links/index.js
+++ .github/actions/check_dead_links/index.js
@@ -1,13 +1,56 @@
 #!/usr/bin/env node
 
+const { Cluster } = require('puppeteer-cluster');
+const markdownLinkExtractor = require('markdown-link-extractor');
 const glob = require("glob");
-const util = require("util");
+const { fsPromises } = require("fs");
+const { readFile } = fsPromises;
 const root = process.env.GITHUB_WORKSPACE;
+const clusterMaxConcurrent = 2;
+const separator = "\n--------------------------------------------------\n";
 
 async function main() {
+
+    console.log(separator);
     console.log(`Searching ${root} for markdown files`);
     const files = await getAllMarkdownFiles(`${root}/**/*.md`);
-    console.log(`All Markdown files:\n${files.join("\n")}`);
+    console.log(`All Markdown files:\n${files.map(stripRoot).join("\n")}`);
+    console.log(separator);
+
+    console.log(
+        `Setup Puppeteer Cluster, max concurrency: ${clusterMaxConcurrent}`);
+
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_BROWSER,
+        maxConcurrency: clusterMaxConcurrent,
+    });
+
+    // Define the only task for this cluster, whose input is a URL to check
+    await cluster.task(async ({ page, data: url }) => {
+      return page.goto(url);
+    });
+
+    let countLinks = 0;
+    const checks = files.map((file) =>
+        readFile(file, { encoding: "utf8" })
+            .then(checkFileForDeadLinks(cluster))
+            .then((links) => {
+                countLinks += links.length;
+                return { file, links };
+            }));
+
+    // Wait for everything to complete.
+    const results = await Promise.all(checks);
+
+    console.log(separator);
+    console.log(`Results complete, counted ${countLinks} links in ${results.length} pages.`)
+
+    results.forEach(({ file, links }, index) => {
+        console.log(`\n    ${index + 1}) ${stripRoot(file)}:`);
+        links.forEach(( link ) => {
+            console.log(`        - ${link.status}: ${link.href}`);
+        });
+    });
 }
 
 const getAllMarkdownFiles = (pattern) => new Promise((resolve, reject) => {
@@ -19,7 +62,27 @@ const getAllMarkdownFiles = (pattern) => new Promise((resolve, reject) => {
 
         resolve(files);
     })
-})
+});
+
+const checkFileForDeadLinks = (cluster) => (markdown) => {
+    const links = markdownLinkExtractor(markdown, true);
+    const checks = links.map(({ href }) => cluster.execute(href));
+    return Promise.all(checks);
+};
+
+const checkMarkdownLinkLivenessInPuppeteer = (link) => {
+
+}
+
+const stripRoot = (path) => path.replace(new RegExp(`^${root}`), "");
+
+
+const puppeteerStart = () => {
+    const browser = await puppeteer.launch();
+    // --no-sandbox from docker image docs:
+    // https://github.com/ianwalter/puppeteer-container
+    const page = browser.launch({ args: ['--no-sandbox'] });
+}
 
 // Start the async entrypoint
 main();
