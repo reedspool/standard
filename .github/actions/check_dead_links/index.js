c9cb0ef6fd38c856e7ce396116055daaee46cee7
Reed Spool <reed@heysparkbox.com>
2021-05-30 22:17:11 -0600

    fix: transform to async await

--- .github/actions/check_dead_links/index.js
+++ .github/actions/check_dead_links/index.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 
 const glob = require("glob");
+const util = require("util");
 const root = process.env.GITHUB_WORKSPACE;
 
-console.log("Searching ${root} for markdown files");
+async function main() {
+    console.log(`Searching ${root} for markdown files`);
+    const files = await getAllMarkdownFiles(`${root}/**/*.md`);
+    console.log(`All Markdown files:\n${files.join("\n")}`);
+}
 
-glob(`${root}/**/*.md`, (error, files) => {
-    if (error) {
-        console.log(`Error: ${error}`);
-        return;
-    }
+const getAllMarkdownFiles = (pattern) => new Promise((resolve, reject) => {
+    glob(pattern, (error, files) => {
+        if (error) {
+            reject(error);
+            return;
+        }
 
-    console.log("Markdown files:\n", files.join("\n"));
+        resolve(files);
+    })
 })
+
+// Start the async entrypoint
+main();
