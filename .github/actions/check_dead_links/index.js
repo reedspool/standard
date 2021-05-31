c322ec063c10c494fe24ce2f7f4a96ede5631288
Reed Spool <reed@heysparkbox.com>
2021-05-30 21:54:41 -0600

    feat: add package.json to build

--- .github/actions/check_dead_links/index.js
+++ .github/actions/check_dead_links/index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log(`Process pwd: ${process.env.PWD}`);
+const glob = require("glob");
+const root = process.env.GITHUB_WORKSPACE;
+
+console.log("Searching ${root} for markdown files");
+
+glob(`${root}/**/*.md`, (error, files) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+
+    console.log("Markdown files:\n", files.join("\n"));
+})
