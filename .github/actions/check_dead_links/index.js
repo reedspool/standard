6dce56b231bfc1afc52e396574d24c22289d4ecd
Reed Spool <reed@heysparkbox.com>
2021-06-08 18:19:26 -0600

    chore: code cleanup and add headers to mimic real browser

--- .github/actions/check_dead_links/index.js
+++ .github/actions/check_dead_links/index.js
@@ -6,8 +6,11 @@ const markdownLinkExtractor = require('markdown-link-extractor');
 const glob = require("glob");
 const { promises: { readFile } } = require("fs");
 const root = process.env.GITHUB_WORKSPACE;
+const repo = process.env.GITHUB_REPOSITORY;
+const sha = process.env.GITHUB_SHA;
 const separator = "\n--------------------------------------------------\n";
-var AsciiTable = require("ascii-table");
+const AsciiTable = require("ascii-table");
+const redX = "\x1b[31m✖\x1b[0m"; // Red-colored unicode "x"
 
 
 // Raise to go faster, limited only by the github action hardware.
@@ -60,11 +63,14 @@ async function main() {
 
         console.log(`\n    ${countFiles++}) ${stripRoot(file)} has ${countFileLinks} links:`);
 
-        completeChecks.forEach(({ value: { status, original } }) => {
+        completeChecks.forEach(({ value }) => {
+            const { status, original, error } = value;
             if (status < 200 || status >= 300) {
-                // Red colored unicode "x"
-                console.log(`        - [\x1b[31m✖\x1b[0m] ${status}: ${original}`);
-                allErrors.push({ status, original, file });
+                console.log(`        - [${redX}] ${status}: ${original}`);
+                if (error) {
+                    console.log(`\n                Puppeteer error:\n`, error);
+                }
+                allErrors.push(value);
             } else {
                 console.log(`        - [✓] ${status}: ${original}`);
             }
@@ -84,7 +90,7 @@ async function main() {
     allErrors.forEach(({ status, original, file }) => {
         const row = [ status, summarizeLink(original), stripRoot(file) ]
         asciiTable.addRow(...row);
-    })
+    });
 
     console.log(asciiTable.toString());
     console.log(separator);
@@ -102,25 +108,44 @@ const getFiles = (pattern) => new Promise((resolve, reject) => {
     })
 });
 
+// Remove the beginning of the path, which is the root of the filesystem
+// for this github action. The path will then be the relative path of the same
+// file within the git repository.
 const stripRoot = (path) => path.replace(new RegExp(`^${root}\\/`), "");
 
-const checkLink = (cluster, file) => async ({ href }) => {
-    const original = href;
+// Use the actual Internet to check if the given URL works.
+const checkLink = (cluster, file) => async ({ url }) => {
+    const original = url;
+    let error;
+
     // If this is not a fully qualified URL, treat it like a relative path
     // within this directory
-    if (! /^https?:\/\//.test(href))
-        href = githubUrlFromPath(resolvePath(file, href));
+    if (! /^https?:\/\//.test(url))
+        url = githubUrlFromPath(resolvePath(file, url));
 
     // First try puppeteer page nav, which throws if it fails
     try {
-        const response = await cluster.execute(href);
+        const response = await cluster.execute(url);
         return { status: response.status(), original };
-    } catch (error) {
-        // Puppeteer nav can fail for a variety of reasons. Try again with
-        // a HEAD check
-        const { ok, status } = await fetch(href, { method: "HEAD" });
-        return { status: status, original };
+    } catch (puppeteerError) {
+        // Do nothing, proceed with the fetch test
+        error = puppeteerError;
     }
+
+    // Puppeteer nav can fail for a variety of reasons. Try again with
+    // a HEAD check
+    const { status } = await fetch(url, {
+        method: "HEAD",
+        headers: {
+            // Use headers to pretend to be a normal browser, to prevent false
+            // negatives when sites try to block robots
+            // From https://stackoverflow.com/a/54361485
+            "Accept-Language": "en,en-US;q=0,5",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+        }
+    });
+    return { status, original, error };
 };
 
 // E.g. from: 'build_process/managing-node-with-brew.md', to: './node.md'
@@ -133,18 +158,18 @@ const resolvePath = (from, to) => {
     return from.replace(/\/?[^/]+$/, `/${to}`);
 }
 
+// From a filepath, compose a URL to that file at this commit in this github repo
 const githubUrlFromPath = (path) => {
-    const repo = process.env.GITHUB_REPOSITORY;
-    const sha = process.env.GITHUB_SHA;
     return `https://github.com/${repo}/blob/${sha}/${path}`;
 }
 
 // Pick a pretty separator for overly long strings
-const summaryEllipsis = "...    ...";
+const summaryEllipsis = "......";
 // Base the max size off a good looking string.
 const summaryLinkMaxLength =
-    ("https://d2mxu07" + summaryEllipsis + "+at+2.24.55+PM.png").length;
+    ("https://d2mxu07abcdefg" + summaryEllipsis + "+at+2.24.55+PM.png").length;
 
+// Splice some sort of ellipsis into really long links to make them shorter
 const summarizeLink = (link) => {
     const len = link.length;
     if (len <= summaryLinkMaxLength) return link;
